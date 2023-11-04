5e2c5cbf4d875053268da05664beba73f2bd1825
Samson Githinji <githinjisamson148@gmail.com>
2023-11-04 08:07:02 +0300

    Display list of bots

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,12 +1,57 @@
 import React from "react";
+import { useGlobalContext } from "../context/botContext";
 
 function BotCollection() {
   // Your code here
+  const { bots } = useGlobalContext();
+
   return (
     <div className="ui four column grid">
       <div className="row">
         {/*...and here..*/}
-        Collection of all bots
+        {/* Collection of all bots */}
+        {bots.map((bot) => {
+          const {
+            armor,
+            avatar_url,
+            bot_class,
+            catchphrase,
+            created_at,
+            damage,
+            health,
+            id,
+            name,
+            updated_at,
+          } = bot;
+          return (
+            <div key={id} className="bot">
+              <div className="bot-img-container">
+                <img src={avatar_url} alt={name} />
+              </div>
+
+              <div className="bot-details">
+                <h2>{name}</h2>
+                <p className="catchphrase">{catchphrase}</p>
+                <p>Class: {bot_class}</p>
+              </div>
+
+              <div className="lower-section">
+                <span>
+                  <i className="fa-solid fa-heart-crack"></i>
+                  {damage}
+                </span>
+                <span>
+                  <i className="fa-solid fa-bolt-lightning"></i>
+                  {health}
+                </span>
+                <span>
+                  <i className="fa-solid fa-shield"></i>
+                  {armor}
+                </span>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
