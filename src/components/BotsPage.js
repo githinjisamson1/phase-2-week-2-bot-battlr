0878eb1e40cca5d7f36b2394471c6de1ca49acbd
Samson Githinji <githinjisamson148@gmail.com>
2023-11-04 08:07:48 +0300

    Style bots

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -1,16 +1,13 @@
-import React from "react";
 import YourBotArmy from "./YourBotArmy";
 import BotCollection from "./BotCollection";
 
 function BotsPage() {
-  //start here with your code for step one
-
   return (
     <div>
       <YourBotArmy />
       <BotCollection />
     </div>
-  )
+  );
 }
 
 export default BotsPage;
