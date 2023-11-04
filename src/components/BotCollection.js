2c45d67b6c50190ab9894be94174b587aadb64c8
Samson Githinji <githinjisamson148@gmail.com>
2023-11-04 16:31:04 +0300

    handleAddToArmy

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,57 +1,66 @@
 import React from "react";
-import { useGlobalContext } from "../context/botContext";
+import { useGlobalBotContext } from "../context/botContext";
+import BotItem from "./BotItem";
 
 function BotCollection() {
-  // Your code here
-  const { bots } = useGlobalContext();
+  // !access value props from BotProvider
+  const { bots, handleAddToArmy } = useGlobalBotContext();
 
   return (
     <div className="ui four column grid">
       <div className="row">
-        {/*...and here..*/}
-        {/* Collection of all bots */}
-        {bots.map((bot) => {
-          const {
-            armor,
-            avatar_url,
-            bot_class,
-            catchphrase,
-            created_at,
-            damage,
-            health,
-            id,
-            name,
-            updated_at,
-          } = bot;
-          return (
-            <div key={id} className="bot">
-              <div className="bot-img-container">
-                <img src={avatar_url} alt={name} />
-              </div>
+        {/* Collection of all bots/conditional rendering/short-circuit */}
+        {bots &&
+          bots.map((bot) => {
+            const {
+              armor,
+              avatar_url,
+              bot_class,
+              catchphrase,
+              damage,
+              health,
+              id,
+              name,
+            } = bot;
 
-              <div className="bot-details">
-                <h2>{name}</h2>
-                <p className="catchphrase">{catchphrase}</p>
-                <p>Class: {bot_class}</p>
-              </div>
+            return (
+              <div key={id} className="bot">
+                <div className="bot-img-container">
+                  <img src={avatar_url} alt={name} />
+                </div>
 
-              <div className="lower-section">
-                <span>
-                  <i className="fa-solid fa-heart-crack"></i>
-                  {damage}
-                </span>
-                <span>
-                  <i className="fa-solid fa-bolt-lightning"></i>
-                  {health}
-                </span>
-                <span>
-                  <i className="fa-solid fa-shield"></i>
-                  {armor}
-                </span>
+                <div className="bot-details">
+                  <h2 className="bot-name">{name}</h2>
+                  <p className="bot-catchphrase">{catchphrase}</p>
+                  <p className="bot-class">
+                    <span>Class: {bot_class}</span>
+                    <button
+                      onClick={() => {
+                        handleAddToArmy(bot);
+                      }}
+                    >
+                      Add to Army
+                    </button>
+                  </p>
+                </div>
+
+                <div className="lower-section">
+                  <span>
+                    <i className="fa-solid fa-heart-crack"></i>
+                    {damage}
+                  </span>
+                  <span>
+                    <i className="fa-solid fa-bolt-lightning"></i>
+                    {health}
+                  </span>
+                  <span>
+                    <i className="fa-solid fa-shield"></i>
+                    {armor}
+                  </span>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
       </div>
     </div>
   );
