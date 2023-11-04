6273668eca948c49b51346c33336a256f6c5c28c
Samson Githinji <githinjisamson148@gmail.com>
2023-11-04 08:05:09 +0300

    Wrap entire app in BotProvider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import "./index.css";
+import { BotProvider } from "./context/botContext";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BotProvider>
+    <App />
+  </BotProvider>
+);
