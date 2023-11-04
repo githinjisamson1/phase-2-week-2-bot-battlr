1c4f45a0a829c6ee2dee69422bc60323600bf215
Samson Githinji <githinjisamson148@gmail.com>
2023-11-04 16:29:45 +0300

    handleReleaseFromArmy, handleDischargeFromDuty

--- src/context/botContext.js
+++ src/context/botContext.js
@@ -5,8 +5,11 @@ const BotContext = React.createContext();
 
 // Provider acting as distributor
 export const BotProvider = ({ children }) => {
-  // !state
+  // !state for bots
   const [bots, setBots] = useState([]);
+
+  // !state for armyBots
+  const [armyBots, setArmyBots] = useState([]);
 
   // !GET
   const fetchAllBots = () => {
@@ -27,10 +30,66 @@ export const BotProvider = ({ children }) => {
   useEffect(() => {
     fetchAllBots();
   }, []);
-  return <BotContext.Provider value={{ bots }}>{children}</BotContext.Provider>;
+
+  // !handle adding a bot to the army
+  const handleAddToArmy = (addToArmy) => {
+    if (!armyBots.includes(addToArmy)) {
+      setArmyBots([...armyBots, addToArmy]);
+      alert("Bot added to army");
+    } else {
+      alert("Bot already in army");
+    }
+
+    // console.log("armyBots:", armyBots);
+  };
+
+  // !release bot from army
+  const handleReleaseFromArmy = (releaseFromArmy) => {
+    if (armyBots.includes(releaseFromArmy)) {
+      const remainingBots = armyBots.filter((armyBot) => {
+        return armyBot.id !== releaseFromArmy.id;
+      });
+      setArmyBots(remainingBots);
+      alert("Bot released from army");
+    } else {
+      alert("Bot not in army");
+    }
+  };
+
+  // !completely discharge bot from duty
+  const handleDischargeFromDuty = (dischargeFromDuty) => {
+    // discharge from army
+    const remainingOnDuty = armyBots.filter((armyBot) => {
+      return armyBot.id !== dischargeFromDuty.id;
+    });
+    setArmyBots(remainingOnDuty);
+
+    // remove from collection
+    const remainingInCollection = bots.filter((bot) => {
+      return bot.id !== dischargeFromDuty.id;
+    });
+    setBots(remainingInCollection);
+
+    alert("Bot discharged from duty");
+  };
+
+  // !value props to distribute values
+  return (
+    <BotContext.Provider
+      value={{
+        bots,
+        armyBots,
+        handleAddToArmy,
+        handleReleaseFromArmy,
+        handleDischargeFromDuty,
+      }}
+    >
+      {children}
+    </BotContext.Provider>
+  );
 };
 
-// will be used globally
-export const useGlobalContext = () => {
+// !will be used globally
+export const useGlobalBotContext = () => {
   return useContext(BotContext);
 };
